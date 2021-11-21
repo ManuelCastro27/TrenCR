7addefd5879abcb2a6949a4eb6695b81eb14378d
Manuel Castro <Manuel Castro@DESKTOP-ECM40SF>
2021-11-20 21:14:54 -0600

    Agregar archivos de proyecto.

---
+++ TransportePublico/Models/Estacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransportePublico.Models
+{
+    public class Estacion
+    {
+        [Key]
+        public int IDEstacion { get; set; }
+
+        public string NombreEstacion { get; set; }
+    }
+}
---
+++ TransportePublico/Models/Ruta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransportePublico.Models
+{
+    public class Ruta
+    {
+        [Key]
+        public int IDRuta { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
